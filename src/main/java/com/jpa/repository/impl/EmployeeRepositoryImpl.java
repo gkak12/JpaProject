7208ae3357cadd6716eb5e77a8488abb954453e4
박현준 <qndjddl@openmate.co.kr>
2023-08-06 20:25:30 +0900

    fetch join 적용

--- src/main/java/com/jpa/repository/impl/EmployeeRepositoryImpl.java
+++ src/main/java/com/jpa/repository/impl/EmployeeRepositoryImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import com.jpa.domain.Employee;
 import com.jpa.domain.EmployeeSearchParam;
 import com.jpa.domain.QEmployee;
+import com.jpa.domain.QGrade;
+import com.jpa.domain.QTeam;
 import com.jpa.repository.EmployeeRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -20,9 +22,13 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
 	@Override
 	public List<Employee> selectList(EmployeeSearchParam employeeSearchParam) {
 		QEmployee employee = QEmployee.employee;
+		QTeam team = QTeam.team;
+		QGrade grade = QGrade.grade;
 
 		List<Employee> list = queryFactory
 								.selectFrom(employee)
+								.join(employee.team, team)
+								.join(employee.grade, grade)
 								.where(employee.id.contains(employeeSearchParam.getId()))
 								.fetch();
 
