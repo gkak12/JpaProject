a55215b1b28b2ada7d6c839c4d305ed49225ef44
박현준 <qndjddl@openmate.co.kr>
2023-08-14 16:36:00 +0900

    Jasypt 적용

---
+++ src/main/java/com/jpa/config/JasyptConfig.java
@@ -0,0 +1,28 @@
+package com.jpa.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+	@Bean("jasyptEncryptorDES")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("test_key");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+}
---
+++ src/test/java/com/jpa/config/JasyptConfigTest.java
@@ -0,0 +1,26 @@
+package com.jpa.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+class JasyptConfigTest {
+
+	@Test
+	void stringEncryptor() {
+		String url = "jdbc:postgresql://localhost:5432/test";
+		String username = "user";
+		String password = "user";
+
+		System.out.println(jasyptEncoding(url));
+		System.out.println(jasyptEncoding(username));
+		System.out.println(jasyptEncoding(password));
+	}
+
+	public String jasyptEncoding(String value) {
+		String key = "test_key";
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+		pbeEnc.setPassword(key);
+		return pbeEnc.encrypt(value);
+	}
+}
